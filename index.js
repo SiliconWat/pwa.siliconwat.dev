78b4b9e22a82eeab8dd22309d3909ab29983c9b6
Thon Ly <thonly@heartbank.ceo>
2023-05-14 07:52:51 -0700

    refactor

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import "/components/tl-footer/element.mjs";
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-4W1QXH6YV9');
+gtag('config', 'G-ZQF252C5QX');
