86f28d7e205c3dea63e08ee906e55958bc87d030
Thon Ly <thonly@siliconwat.dev>
2023-09-12 16:17:49 -0700

    remove nhia kou

--- components/tl-header/template.mjs
+++ components/tl-header/template.mjs
@@ -3,19 +3,19 @@ const template = document.createElement("template");
 template.innerHTML = `
     <link rel="stylesheet" href="/components/tl-header/shadow.css">
     <ul>
-        <li><a href="/">333™</a></li>
-        <li><a href="/thonly">THonly™</a></li>
+        <li><a href="/">Thon Ly</a></li>
         <li><a href="/siliconwat">Silicon Wat LLC</a></li>
-        <li><a href="/nhiakou">Nhia Kou Ltd</a></li>
+        <li><a href="/333">333 Ltd</a></li>
+        <li><a href="/thonly">THonly™</a></li>
         <li><a href="/heartbank">HeartBank®</a></li>
     </ul>
     <nav>
         <h3 onclick="this.getRootNode().host.menu()">Menu</h3> 
         <select onchange="this.getRootNode().host.page(this)">
-            <option value="/">333™</option>
-            <option value="/thonly/">THonly™</option>
+            <option value="/">Thon Ly</option>
             <option value="/siliconwat/">Silicon Wat LLC</option>
-            <option value="/nhiakou/">Nhia Kou Ltd</option>
+            <option value="/333/">333 Ltd</option>
+            <option value="/thonly/">THonly™</option>
             <option value="/heartbank/">HeartBank®</option>
         </select>
     </nav>
