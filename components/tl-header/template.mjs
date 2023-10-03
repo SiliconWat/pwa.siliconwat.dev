cf8ee45c9e9065c27d0ea895bdca1fc2beb8716d
Thon Ly <thonly@siliconwat.dev>
2023-10-02 17:43:32 -0700

    refactor

--- components/tl-header/template.mjs
+++ components/tl-header/template.mjs
@@ -3,20 +3,20 @@ const template = document.createElement("template");
 template.innerHTML = `
     <link rel="stylesheet" href="/components/tl-header/shadow.css">
     <ul>
-        <li><a href="/">Thon Ly</a></li>
+        <li><a href="/">Thon Ly Trust</a></li>
         <li><a href="/siliconwat">Silicon Wat LLC</a></li>
         <li><a href="/333">333 Ltd</a></li>
-        <li><a href="/thonly">THonly™</a></li>
-        <li><a href="/heartbank">HeartBank®</a></li>
+        <li><a href="/thonly">THonly™ LLC</a></li>
+        <li><a href="/heartbank">HeartBank® Nonprofit</a></li>
     </ul>
     <nav>
         <h3 onclick="this.getRootNode().host.menu()">Menu</h3> 
         <select onchange="this.getRootNode().host.page(this)">
-            <option value="/">Thon Ly</option>
+            <option value="/">Thon Ly Trust</option>
             <option value="/siliconwat/">Silicon Wat LLC</option>
             <option value="/333/">333 Ltd</option>
-            <option value="/thonly/">THonly™</option>
-            <option value="/heartbank/">HeartBank®</option>
+            <option value="/thonly/">THonly™ LLC</option>
+            <option value="/heartbank/">HeartBank® Nonprofit</option>
         </select>
     </nav>
 `;
